821c2e598f02cea4128d077108627bdd48f56902
fazalrai <fazal.rai456@gmail.com>
2021-05-30 21:26:31 +0500

    after reseting two commits in second branch

---
+++ src/components/Counter1.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Counter1() {
+  return (
+    <>
+    <h1>Counter 1</h1>
+    <h1>Counter 2</h1>
+    <h1>Counter 3</h1>
+
+    </>
+
+  )
+}
+
+export default Counter1
